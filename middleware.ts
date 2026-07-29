5684fe3e7c40c3cea37cf2daf5583703a250cc03
codezerosx1 <no-reply@github.com>
2026-07-29 02:59:17 -0400

    fix(middleware): use named middleware export

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server"
 
-export default function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const protectedRoutes = {
     "/cliente": "cliente",
     "/tecnico": "tecnico",
